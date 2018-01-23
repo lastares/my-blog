086c4e90b7f1fd47102350d1940e35736e644374
songyaofeng <songyaofeng@kuaiqiangche.com>
2018-01-23 19:10:57 +0800

    update qq login

--- config/app.php
+++ config/app.php
@@ -169,7 +169,8 @@ return [
         /*
          * Package Service Providers...
          */
-        Laravel\Socialite\SocialiteServiceProvider::class,
+        // Laravel\Socialite\SocialiteServiceProvider::class,
+        \SocialiteProviders\Manager\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
