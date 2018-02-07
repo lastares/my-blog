491f23e2fc672e3bf35a1b9a570d7e94b2592f6d
songyaofeng <songyaofeng@kuaiqiangche.com>
2018-02-07 16:18:50 +0800

    添加文章评论功能

--- public/home/js/comment.js
+++ public/home/js/comment.js
@@ -106,7 +106,7 @@ function comment(obj){
                   $.post(ajaxCommentUrl, postData, function(data) {
                       console.log(data);
                       if(data.code === 1) {
-                          layer.msg(data.message);
+                          layer.msg(data.message, {icon: 2});
                       }
                       var newPid=data.id;
                       var replyName=$(obj).attr('username');
