27fcd719acc1fc996c1799a72127b2299e95cf61
songyaofeng <songyaofeng@kuaiqiangche.com>
2018-02-07 19:44:58 +0800

    添加文章评论功能

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -202,6 +202,7 @@ class IndexController extends BaseController
             $oauthUserId = app('db')->table('comments')->where('pid', $pid)->value('oauth_user_id');
             if($oauthUserId == $userId) {
                 $data['type'] = 'yes';
+                return response()->json(['code' => 1, 'message' => '自己不能评论自己']);
             }
         }
 //        if($pid == $userId) {
