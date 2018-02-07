5ec44bf378c7c536d18bae46eb4226cc73dfaa0d
songyaofeng <songyaofeng@kuaiqiangche.com>
2018-02-07 17:25:56 +0800

    添加文章评论功能

--- resources/views/home/index/article.blade.php
+++ resources/views/home/index/article.blade.php
@@ -219,7 +219,7 @@
                 <li class="bg-color">
                     <div class="comment-ava">
                         <a href="#" id="Comment-137" target="_blank" rel="nofollow" title="{{ $v1['name'] }}">
-                            <img class="img-circle" src="{{ $v['avatar'] }}" alt="{{ $v1['name'] }}"/></a>
+                            <img class="img-circle" src="{{ $v1['avatar'] }}" alt="{{ $v1['name'] }}"/></a>
                         <!--<span><i class="el-user"></i>木杉</span>-->
                     </div>
                     <div class="comment-info ">
