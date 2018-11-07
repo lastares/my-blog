c99f6b9298935f7196b575961a7f33f8fe9a133a
songyaofeng <songyaofeng@aliyun.com>
2018-11-07 17:16:30 +0800

    [feat:]解决图片存储问题

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -57,12 +57,12 @@ class ArticleController extends Controller
     public function uploadImage()
     {
         $result = upload('editormd-image-file', 'uploads/article');
-        $imgRootUrl = '//admin.songyaofeng.com';
+        //$imgRootUrl = '//admin.songyaofeng.com';
         if ($result['status_code'] === 200) {
             $data = [
                 'success' => 1,
                 'message' => $result['message'],
-                'url' => $imgRootUrl . $result['data']['path'] . $result['data']['new_name']
+                'url' => $result['data']['path'] . $result['data']['new_name']
             ];
         } else {
             $data = [
